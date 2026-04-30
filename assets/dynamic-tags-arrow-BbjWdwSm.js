da687a54601b48442f5d37b1cd83351866ee77bc
AlinaShamina <a.shamina@quadient.com>
2026-04-30 16:31:14 +0200

    deploy: aging-report
    Source: v2-front@825aa7f85 on prototype/settings
    
    Built: 2026-04-30T14:30:17Z

---
+++ assets/dynamic-tags-arrow-BbjWdwSm.js
@@ -0,0 +1,1 @@
+import{c as a}from"./index.esm-Dx6LK5Qb.js";import"./index-8CofC8RP.js";const o={name:"dynamic-tags-arrow",data:{width:"10",height:"10",viewBox:"0 0 10 10",data:'<path pid="0" d="M8.7 2.2c.3-.3.8-.3 1 0 .4.4.4.9 0 1.2l-4 4.4c-.3.3-.9.3-1.2 0L.2 3.4a.8.8 0 0 1 0-1.2c.3-.3.8-.3 1.1 0L5 6l3.7-3.8z"/>',originalColors:[],stopColors:[]}},c=a(o);export{c as default};
